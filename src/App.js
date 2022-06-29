4c5cc84f05fd832f6cc9b330b42c866664bf9e6b
Putra Iskandar <putraiskandarmuda78@gmail.com>
2022-06-29 17:32:26 +0700

    http get request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
-
+      <PostList />
     </div>
   );
 }
